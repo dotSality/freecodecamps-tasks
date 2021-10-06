d9cc53211ca4e53948f7a721fe45483d8929280c
dotSality <90727519+dotSality@users.noreply.github.com>
2021-10-06 21:10:56 +0300

    added few paragraphs

--- loops/js/app.js
+++ loops/js/app.js
@@ -29,11 +29,51 @@
 // }
 // // Only change code below this line
 
-// Setup
-var myArr = [ 2, 3, 4, 5, 6];
-let total = 0;
-for (let i = 0; i < myArr.length; i++) {
-    total += myArr[i];
-}
-// Only change code below this line
+// // Setup
+// var myArr = [ 2, 3, 4, 5, 6];
+// let total = 0;
+// for (let i = 0; i < myArr.length; i++) {
+//     total += myArr[i];
+// }
+// // Only change code below this line
+//
 
+
+// function multiplyAll(arr) {
+//     var product = 1;
+//     // Only change code below this line
+//     for (let i = 0; i < arr.length; i++) {
+//         for(let j = 0; j < arr[i].length; j++) {
+//             product *= arr[i][j];
+//         }
+//     }
+//     // Only change code above this line
+//     return product;
+// }
+//
+// multiplyAll([[1,2],[3,4],[5,6,7]]);
+
+// Setup
+// var myArray = [];
+// var i = 10;
+// do {
+//     myArray.push(i);
+//     i++;
+// }
+// // Only change code below this line
+// while (i < 11)
+
+// function sum(arr, n) {
+//     // Only change code below this line
+//     let product = 0;
+//     for (let i = 0; i < n; i++) {
+//         product += arr[i]
+//     }
+//     // Only change code above this line
+// }
+
+function sum(arr, n) {
+    // Only change code below this line
+    return n <= 0 ? 0 : sum(arr,n-1)+arr[n-1];
+    // Only change code above this line
+}
