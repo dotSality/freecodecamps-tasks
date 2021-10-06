6a067f6a3921fd0f32e3c500befb331f8aff685c
dotSality <90727519+dotSality@users.noreply.github.com>
2021-10-06 12:19:31 +0300

    updated tasks

--- many options (case)/js/app.js
+++ many options (case)/js/app.js
@@ -76,29 +76,60 @@
 //
 // sequentialSizes(1);
 //*******************************************************
-function chainToSwitch(val) {
-    let answer = "";
-    // Only change code below this line
-    switch (val) {
-        case "bob" :
-            answer = "Marley";
-            break;
-        case 42:
-            answer = "The Answer";
-            break;
-        case 1:
-            answer = "There is no #1";
-            break;
-        case 99:
-            answer = "Missed me by this much!";
-            break;
-        case 7:
-            answer = "Ate Nine";
-            break;
+// function chainToSwitch(val) {
+//     let answer = "";
+//     // Only change code below this line
+//     switch (val) {
+//         case "bob" :
+//             answer = "Marley";
+//             break;
+//         case 42:
+//             answer = "The Answer";
+//             break;
+//         case 1:
+//             answer = "There is no #1";
+//             break;
+//         case 99:
+//             answer = "Missed me by this much!";
+//             break;
+//         case 7:
+//             answer = "Ate Nine";
+//             break;
+//
+//     }
+//     // Only change code above this line
+//     return answer;
+// }
+//
+// chainToSwitch(7);
+//********************************************************
+var count = 0;
 
+function cc(card) {
+    // Only change code below this line
+    switch(card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count ++;
+            break;
+        case 10:
+        case 'J':
+        case 'Q':
+        case 'K':
+        case 'A':
+            count --;
+            break;
     }
+    if (count > 0) {
+        return count + " Bet";
+    } else {
+        return count + " Hold";
+    }
+
     // Only change code above this line
-    return answer;
 }
 
-chainToSwitch(7);
+cc(2); cc(3); cc(7); cc('K'); cc('A');
