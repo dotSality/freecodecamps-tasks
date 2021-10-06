793cfaa3d693e90d794be1c1320e477cb3a9e4b6
dotSality <90727519+dotSality@users.noreply.github.com>
2021-10-06 18:25:14 +0300

    added loops tasks

---
+++ loops/js/app.js
@@ -0,0 +1,39 @@
+// // Setup
+// var myArray = [];
+// let i = 5;
+// while (i>=0) {
+//     myArray.push(i);
+//     i--;
+// }
+// // Only change code below this line
+
+// // Setup
+// var myArray = [];
+// for (i = 1; i < 6; i++) {
+//     myArray.push(i)
+// }
+// // Only change code below this line
+
+// // Setup
+// var myArray = [];
+// for (let i = 1; i < 10; i+=2) {
+//     myArray.push(i);
+// }
+// // Only change code below this line
+
+// // Setup
+// var myArray = [];
+// for (let i = 9; i > 0; i -= 2) {
+//     console.log(i);
+//     myArray.push(i);
+// }
+// // Only change code below this line
+
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+    total += myArr[i];
+}
+// Only change code below this line
+
