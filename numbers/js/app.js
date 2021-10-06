85c83d8d4205d9d8ad5751fb97e74f5c036302b0
dotSality <90727519+dotSality@users.noreply.github.com>
2021-10-06 23:09:00 +0300

    numbers
    solved numbers tasks

---
+++ numbers/js/app.js
@@ -0,0 +1,34 @@
+// function randomFraction() {
+//
+//     // Only change code below this line
+//
+//     return Math.random();
+//
+//     // Only change code above this line
+// }
+
+// function randomWholeNum() {
+//
+//     // Only change code below this line
+//
+//     return Math.floor(Math.random()*10)
+// }
+
+// function randomRange(myMin, myMax) {
+//     // Only change code below this line
+//     return Math.floor(Math.random()*(myMax - myMin +1)+myMin);
+//     // Only change code above this line
+// }
+
+// function convertToInteger(str) {
+//     return parseInt(str);
+// }
+//
+// convertToInteger("56");
+
+// function convertToInteger(str) {
+//     return parseInt(str, 2);
+// }
+//
+// convertToInteger("10011");
+
